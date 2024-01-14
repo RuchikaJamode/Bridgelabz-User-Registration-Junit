d5e8d09ce77eb68cafec5335d9c875010ad1dcc4
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-14 18:18:58 +0530

    user last name valid or not

--- UserRegistrationPrograms.java
+++ UserRegistrationPrograms.java
@@ -8,5 +8,11 @@ public class UserRegistrationPrograms {
         // Use String.matches() to check if the input matches the regular expression
         return firstName.matches(regex);
     }
+	public boolean isValidLastName(String lastName) {
+        // Regular expression for validating the first name
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
 
+        // Use String.matches() to check if the input matches the regular expression
+        return lastName.matches(regex);
+    }
 }

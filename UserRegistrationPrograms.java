83188ca269b7c151e16d867d80787e9886b28e51
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-14 18:06:10 +0530

    welcome to user registration programs using junit

---
+++ UserRegistrationPrograms.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.junit.userregistration;
+
+public class UserRegistrationPrograms {
+	public boolean isValidFirstName(String firstName) {
+        // Regular expression for validating the first name
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+        // Use String.matches() to check if the input matches the regular expression
+        return firstName.matches(regex);
+    }
+
+}
